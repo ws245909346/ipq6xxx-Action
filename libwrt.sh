a1bd99fe6146faa2f1558921c668dd45f63d37d4
ws245909346 <102574232+ws245909346@users.noreply.github.com>
2025-08-16 21:28:57 +0800

    istore

--- libwrt.sh
+++ libwrt.sh
@@ -6,7 +6,10 @@ chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 
 # iStore
-echo >> feeds.conf.default
-echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-./scripts/feeds update istore
-./scripts/feeds install -d y -p istore luci-app-store
+# echo >> feeds.conf.default
+# echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
+# ./scripts/feeds update istore
+# ./scripts/feeds install -d y -p istore luci-app-store
+
+git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
+git_sparse_clone main https://github.com/linkease/istore luci
